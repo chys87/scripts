9f834410dc85758174d3ac1f9f96de5890cc6a92
chys <admin@chys.info>
2013-07-25 23:01:39 +0800

    Minor tweak to the license

--- idfinal.py
+++ idfinal.py
@@ -15,7 +15,7 @@
 #   list of conditions and the following disclaimer in the documentation and/or
 #   other materials provided with the distribution.
 #
-#   Neither the name of chys <admin@CHYS.INFO> nor the names of its
+#   Neither the name of chys <admin@CHYS.INFO> nor the names of other
 #   contributors may be used to endorse or promote products derived from
 #   this software without specific prior written permission.
 #
