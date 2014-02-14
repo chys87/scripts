00444615b241bb8b8ec5436459bd67850e132326
chys <admin@chys.info>
2014-02-14 15:38:29 +0800

    Add json_pprint.py

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 * `detailed_size.py`: 增强版 [size](http://linux.die.net/man/1/size)
 * `gitconfig`: 我的 [Git 配置文件](https://www.kernel.org/pub/software/scm/git/docs/git-config.html)
 * `idfinal.py` (Python 2.7/3.2): 计算或验证中国身份证号码的最后一位
+* `json_pprint.py` (Python 2.7/3.2): 简单粗暴的 JSON 格式化工具，支持一部分非标准 JSON 格式
 * `mdqp.py` (Python 2.7/3.3): [Markdown](http://en.wikipedia.org/wiki/Markdown) 格式文档快速查看器
 * `salary-calc.py` (Python 3.2+): 一个简单的中国社保、个人所得税计算器
 * `shellrc`: 我的 shell 配置文件 (bash/[zsh](http://www.zsh.org) 通用)
@@ -26,6 +27,7 @@
 * `detailed_size.py`: Improved version of [size](http://linux.die.net/man/1/size)
 * `gitconfig`: My [Git config file](https://www.kernel.org/pub/software/scm/git/docs/git-config.html)
 * `idfinal.py` (Python 2.7/3.2): Calculate or verify the last digit of a Chinese ID number
+* `json_pprint.py` (Python 2.7/3.2): Quick and dirty JSON pretty printer, supporing some nonstandard JSON
 * `mdqp.py` (Python 2.7/3.3): A quick previewer for [Markdown](http://en.wikipedia.org/wiki/Markdown) documentation
 * `salary-calc.py` (Python 3.2+): A simple Chinese social security and income tax calculator
 * `shellrc`: My shell configuration file (compatible with both bash and [zsh](http://www.zsh.org))
---
+++ json_pprint.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:ts=8 sts=4 sw=4 expandtab ft=python
+
+#
+# Copyright (c) 2014, chys <admin@CHYS.INFO>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#   Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+#   Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+#   Neither the name of chys <admin@CHYS.INFO> nor the names of other
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+
+import json
+import pprint
+import re
+import sys
+
+
+def fix_json(src):
+    # I have to deal with a lot of non-standard JSON.
+    # Try to fix them whenever possible.
+
+    # Fix property names missing quotes
+    src = re.sub(r'([\{,]\s*)(\w+)(?=\s*:)', r'\1"\2"', src)
+    return src
+
+
+def main():
+    src = sys.stdin.read()
+    try:
+        data = json.loads(src, strict=False)
+    except ValueError:
+        data = None
+    if not data:
+        data = json.loads(fix_json(src), strict=False)
+    pprint.pprint(data)
+#    dst = json.dumps(data, ensure_ascii=False, indent=4)
+#    print(dst)
+
+
+if __name__ == '__main__':
+    main()
